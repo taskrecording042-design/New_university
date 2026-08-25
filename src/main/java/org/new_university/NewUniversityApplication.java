6b3913c585eb7cf603f3b29711508e623205cc3f
taskrecorder <task.recording.046@gmail.com>
2026-08-26 00:03:40 +0300

    Created a new notification branch

--- src/main/java/org/new_university/NewUniversityApplication.java
+++ src/main/java/org/new_university/NewUniversityApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewUniversityApplication {
 
+	// So i will use this branch to set up the notifications feature
+
 	public static void main(String[] args) {
 		SpringApplication.run(NewUniversityApplication.class, args);
 	}
