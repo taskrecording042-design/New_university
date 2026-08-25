a11818d5abe98724be0f51fc519e95afc60e311c
taskrecorder <task.recording.046@gmail.com>
2026-08-25 23:54:42 +0300

    Initial commit

---
+++ src/main/java/org/new_university/NewUniversityApplication.java
@@ -0,0 +1,13 @@
+package org.new_university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/new_university/NewUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.new_university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
